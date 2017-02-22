87d30fe8c136a24076f44a32658629b570580c73
Przemysław Krasiński <przemek.krasinski@gmail.com>
2017-02-22 22:36:28 +0100

    Revert "Add animation class"
    This reverts commit 0b0cd4b45c03c8480e2f2d79304330e1f316bb45.

--- AmazingScrollLibrary/amazingScroll.js
+++ AmazingScrollLibrary/amazingScroll.js
@@ -1,66 +1,22 @@
 "use strict";
 
- var asElement = function (element, animation) {
-    let self = {};
-
-    self.cl = function (){
-        console.log("działa");
-    }
-
-    return self;
-}
-
-var animationSet = new Set();
-
-var animation = function (data) {
-    var self = {},
-        startStyle = [],
-        finishStyle = [];
-
-    self.init = function () {
-        let tab = [];
-        tab = data.split(',');
-        tab.forEach(function(e) {
-            let styleS = {},
-                styleF = {};
-            styleS["element"] = e.split(":")[0].replace(/\s*/gi, "");
-            styleS["value"] = e.split(":")[1].split("->")[0];
-            if(styleS.element !== undefined && styleS.value !== undefined)
-                startStyle.push(styleS);
-
-            styleF["element"] = e.split(":")[0].replace(/\s*/gi, "");
-            styleF["value"] = e.split(":")[1].split("->")[1];
-            if(styleF.element !== undefined && styleF.value !== undefined)
-                finishStyle.push(styleF);
-        });
-        animationSet.add(this);
-    }
-
-    self.getStartStyle = function() {
-        return startStyle;
-    }
-
-    self.init();
-
-    return self;
-}
-
 if (document.querySelector('[data-as]')) {
     document.querySelectorAll('[data-as]').forEach(function(e) {
-        var data = e.dataset.as;
-        var data2 = e.dataset.as.split(",");
-        var anim = new animation(data);
-        var startElement = anim.getStartStyle();
+        var data = e.dataset.as.split(',');
+        console.log(data);
         if ((e.getBoundingClientRect().top / window.innerHeight) >= 1) {
-            for (var i = 0; i < startElement.length; i++) {
-                e.style[startElement[i].element] = startElement[i].value;
+            for (var i = 0; i < data.length; i++) {
+                var el = data[i].split(":")[0].replace(/\s*/gi, "");
+                var val1 = data[i].split(":")[1].split("->")[0];
+                var val2 = data[i].split(":")[1].split("->")[1];
+                e.style[el] = val1;
             }
             window.addEventListener('scroll', function() {
                 if ((e.getBoundingClientRect().top / window.innerHeight) < 0.75) {
-                    for (var i = 0; i < data2.length; i++) {
-                        var el = data2[i].split(":")[0].replace(/\s*/gi, "");
-                        var val1 = data2[i].split(":")[1].split("->")[0];
-                        var val2 = data2[i].split(":")[1].split("->")[1];
+                    for (var i = 0; i < data.length; i++) {
+                        var el = data[i].split(":")[0].replace(/\s*/gi, "");
+                        var val1 = data[i].split(":")[1].split("->")[0];
+                        var val2 = data[i].split(":")[1].split("->")[1];
                         if (val2 !== undefined)
                             e.style[el] = val2;
                         else
